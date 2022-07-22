c4f1a4af1c2944544fc45c8e9e72549405ed15fa
Oleh Nikulin <oleh.nikulin.dev@gmail.com>
2022-07-22 18:29:24 +0300

    added arrow, try to create select

--- src/index.js
+++ src/index.js
@@ -4,3 +4,15 @@ import 'bootstrap';
 import * as bootstrap from 'bootstrap';
 import 'normalize.css';
 import './styles/main.scss';
+
+// const dropdown = document.querySelector('.dropdown-el');
+// dropdown.addEventListener('click', function (e) {
+//   e.preventDefault();
+//   e.stopPropagation();
+//   this.classList.toggle('expanded');
+//   const attFor = e.target.getAttribute('for');
+//   const selectedItem = document.querySelector(`#${attFor}`);
+//   if (selectedItem) selectedItem.setAttribute('checked', true);
+// });
+
+// document.addEventListener('click', () => dropdown.classList.remove('expanded'));
