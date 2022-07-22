14a7215b40f7c282ba87c19160b86dfdc1275639
Oleh Nikulin <oleh.nikulin.dev@gmail.com>
2022-07-22 11:29:53 +0300

    created inital setup

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import '@popperjs/core';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import * as bootstrap from 'bootstrap';
+import 'normalize.css';
+import './styles/main.scss';
