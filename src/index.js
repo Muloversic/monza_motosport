d5924ae4a73e14d3ccedcc61089609e3d443eb80
Oleh Nikulin <oleh.nikulin.dev@gmail.com>
2022-07-23 10:18:01 +0300

    modified script that it keeps default value and add selected class; added all drop downs, changed their width

--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ import * as bootstrap from 'bootstrap';
 import 'normalize.css';
 import './styles/main.scss';
 
-
-const yearMenu = document.querySelectorAll('.dropdown-menu');
-[...yearMenu].forEach((menu) => {
+const selectMenu = document.querySelectorAll('.dropdown-menu');
+[...selectMenu].forEach((menu) => {
   const menuType = menu.getAttribute('aria-labelledby');
   menu.addEventListener('click', (e) => {
     const text = e.target.textContent;
     const targetButton = document.querySelector(`#${menuType}`);
+    const defaultText = targetButton.textContent;
     targetButton.textContent = text;
+    e.target.textContent = defaultText;
+    targetButton.classList.add('selected');
   });
 });
